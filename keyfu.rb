bdbc7f02fbd0bb365dee2366b0c802c95e47863a
Silas Sewell <silas@sewell.org>
2014-09-27 13:13:52 -0400

    keyfu: 0.2.0

--- keyfu.rb
+++ keyfu.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Keyfu < Formula
   homepage 'http://www.keyfu.com'
-  url 'https://github.com/silas/keyfu/releases/download/0.1.0/keyfu_darwin_amd64.zip'
-  sha256 '81b796d1a560a919138a45bd60a5d2d0f7f1f24ce8bb54e20681ddd61cd9e126'
-  version '0.1.0'
+  url 'https://github.com/silas/keyfu/releases/download/0.2.0/keyfu_darwin_amd64.zip'
+  sha256 'f67e1097ae2e5b5b46ef1db61a071d387ba7aa229f51e10dc2f4510b895dde3b'
+  version '0.2.0'
 
   depends_on :arch => :x86_64
 
   def install
-    bin.install 'keyfu_darwin_amd64' => 'keyfu'
+    bin.install 'keyfu'
   end
 end
