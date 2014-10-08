fe26a85c65714470db8bd1e5012afd89e70dbe6a
Silas Sewell <silas@sewell.org>
2014-10-08 12:11:10 -0700

    Update dot.rb

--- dot.rb
+++ dot.rb
@@ -4,7 +4,7 @@ class Dot < Formula
   homepage "https://github.com/silas/dot"
   url "https://raw.githubusercontent.com/silas/dot/v0.1.0/dot"
   sha256 "ae3a8fe487d2333a05ce688c0a42262c9574454a76caab82e98d1ebe2e43b867"
-  version '0.1.0'
+  version "0.1.0""
 
   def install
     bin.install "dot"
